90c57e63a034fe348bd340e1f205c4d878c60b25
Sergey <persalius@gmail.com>
2019-12-17 11:22:14 +0200

    edit

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+    return config;
+}
+
+// const {compose} = require("react-app-rewired");
+//
+// module.exports = compose();
